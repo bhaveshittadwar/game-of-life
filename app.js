7aa70029eba42eb4145021e979d57c62e447bfa6
Bhavesh Ittadwar <bhavesh.ittadwar@michelin.com>
2021-05-28 00:25:27 +0530

    manage static assets

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 // set 'ejs' as the view engine
 app.set('view engine', 'ejs')
